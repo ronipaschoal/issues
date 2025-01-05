6339e47b8837f0741589a4c6a1b57f4a3b67b6aa
Roni Magozzo Paschoal <ronipaschoal@gmail.com>
2025-01-05 17:51:18 -0300

    refactor: limit size on web

--- lib/ui/core/widgets/ui_scaffold_widget.dart
+++ lib/ui/core/widgets/ui_scaffold_widget.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:issues/domain/models/theme_model.dart';
-import 'package:issues/ui/core/widgets/flutter_banner_widget.dart';
+import 'package:issues/utils/flutter_banner/flutter_banner_widget.dart';
 
 class UiScaffoldWidget extends StatelessWidget {
   final String title;
@@ -18,43 +19,54 @@ class UiScaffoldWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return FlutterBannerWidget(
-      child: Scaffold(
-        appBar: title.isEmpty
-            ? null
-            : AppBar(
-                backgroundColor: theme.colorScheme.primary.withAlpha(30),
-                surfaceTintColor: Colors.transparent,
-                centerTitle: false,
-                title: Text(
-                  title,
-                  style: theme.textTheme.titleLarge,
-                  textAlign: TextAlign.start,
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: kIsWeb ? 400 : double.infinity,
+            maxHeight: kIsWeb ? 800 : double.infinity,
+          ),
+          child: FlutterBannerWidget(
+            child: Scaffold(
+              appBar: title.isEmpty
+                  ? null
+                  : AppBar(
+                      backgroundColor: theme.colorScheme.primary.withAlpha(30),
+                      surfaceTintColor: Colors.transparent,
+                      centerTitle: false,
+                      title: Text(
+                        title,
+                        style: theme.textTheme.titleLarge,
+                        textAlign: TextAlign.start,
+                      ),
+                      systemOverlayStyle: ThemeEnum.values.firstWhere((locale) {
+                        return theme.brightness == locale.brightness;
+                      }).systemUiOverlayStyle,
+                    ),
+              body: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomCenter,
+                    stops: const [
+                      0.1,
+                      0.2,
+                      0.7,
+                    ],
+                    colors: [
+                      Theme.of(context).colorScheme.primary.withAlpha(30),
+                      Theme.of(context).colorScheme.primary.withAlpha(30),
+                      Theme.of(context).colorScheme.surface.withAlpha(30),
+                    ],
+                  ),
                 ),
-                systemOverlayStyle: ThemeEnum.values.firstWhere((locale) {
-                  return theme.brightness == locale.brightness;
-                }).systemUiOverlayStyle,
+                child: SafeArea(child: body),
               ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomCenter,
-              stops: const [
-                0.1,
-                0.2,
-                0.7,
-              ],
-              colors: [
-                Theme.of(context).colorScheme.primary.withAlpha(30),
-                Theme.of(context).colorScheme.primary.withAlpha(30),
-                Theme.of(context).colorScheme.surface.withAlpha(30),
-              ],
+              bottomNavigationBar: bottomNavigationBar,
             ),
           ),
-          child: SafeArea(child: body),
         ),
-        bottomNavigationBar: bottomNavigationBar,
       ),
     );
   }
